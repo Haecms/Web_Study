e42f9aa8933a5cedbc8ff1d4eaa446d0040ff9d5
Haecms <whgo12@naver.com>
2023-08-31 00:48:26 +0900

    웹 공부 커밋

--- hc_python - jupyter notebook/Flask/flask_basic.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "조해찬 입니다 ㅍㅋㅍㅋ"
-
-app.run(host="192.168.0.112", port=5000, debug=True)
--- hc_python - jupyter notebook/Flask/static/hello.js
+++
@@ -1,3 +0,0 @@
-function sayHello(a){
-    alert(a+"를 입력받았습니다.")
-}
